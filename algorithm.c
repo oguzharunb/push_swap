861245aed4ff9f79bd96dacd6a243b5b888ea703
oguzharunb <90613727+oguzharunb@users.noreply.github.com>
2025-01-11 19:25:03 +0300

    edited for norminette

--- algorithm.c
+++ algorithm.c
@@ -6,7 +6,7 @@
 /*   By: obastug <obastug@student.42kocaeli.com.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 14:51:06 by obastug           #+#    #+#             */
-/*   Updated: 2025/01/11 18:54:25 by obastug          ###   ########.fr       */
+/*   Updated: 2025/01/11 19:24:39 by obastug          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,190 +17,128 @@
 // it will return index 3 (5 will be pushed to 4 from 3)
 
 // push excluded
-unsigned long	calculate_number_of_ops(int a_size, int ia, int b_size, int ib)
+unsigned long	min_number_of_ops(int a_size, int ia, int b_size, int ib)
 {
-	int				b_diff_rr;
-	int				a_diff_rr;
-	int				rr_optimized;
-	int				r_optimized;
+	int				b_diff;
+	int				a_diff;
+	int				rr_use;
+	int				r_use;
 	unsigned long	ret;
 
-	a_diff_rr = a_size - ia;
-	b_diff_rr = b_size - ib;
-	rr_optimized = a_diff_rr;
-	if (b_diff_rr > a_diff_rr)
-		rr_optimized = b_diff_rr;
-	r_optimized = ia;
+	a_diff = a_size - ia;
+	b_diff = b_size - ib;
+	rr_use = a_diff;
+	if (b_diff > a_diff)
+		rr_use = b_diff;
+	r_use = ia;
 	if (ib > ia)
-		r_optimized = ib;
-	ret = min_of_4(r_optimized, rr_optimized, ia + b_diff_rr, ib + a_diff_rr);
-	ret += which_one_is_min(r_optimized, rr_optimized, ia + b_diff_rr, ib + a_diff_rr) << 32;
+		r_use = ib;
+	ret = min_of_4(r_use, rr_use, ia + b_diff, ib + a_diff);
+	ret += which_one_is_min(r_use, rr_use, ia + b_diff, ib + a_diff) << 32;
 	return (ret);
 }
 
 // it returns correctly till its a sorted linked list
-int	find_place_for_number_in_list(t_node *stack_b, int number)
+int	find_place_for_number_in_list(t_node *s_b, int number)
 {
 	t_node	*temp;
 	int		order;
 
-	if (number == stack_b->data || last_node(stack_b)->data == number || (last_node(stack_b)->data >= stack_b->data && number > stack_b->data && number < last_node(stack_b)->data) || (last_node(stack_b)->data < stack_b->data && (number < last_node(stack_b)->data || number > stack_b->data)))
+	if (number == s_b->data || last_node(s_b)->data == number
+		|| (last_node(s_b)->data >= s_b->data && number > s_b->data
+			&& number < last_node(s_b)->data) || (last_node(s_b)->data
+			< s_b->data && (number < last_node(s_b)->data
+				|| number > s_b->data)))
 		return (0);
 	order = 0;
-	while (stack_b->next)
+	while (s_b->next)
 	{
 		order++;
-		if (number == stack_b->next->data || stack_b->data == number || (stack_b->data >= stack_b->next->data && number > stack_b->next->data && number < stack_b->data) || (stack_b->data < stack_b->next->data && (number < stack_b->data || number > stack_b->next->data)))
-			break;
-		stack_b = stack_b->next;
+		if (number == s_b->next->data || s_b->data == number
+			|| (s_b->data >= s_b->next->data && number
+				> s_b->next->data && number < s_b->data)
+			|| (s_b->data < s_b->next->data && (number
+					< s_b->data || number > s_b->next->data)))
+			break ;
+		s_b = s_b->next;
 	}
 	return (order);
 }
 
 //1-rr, 2-rrr, 3-ra:rrb, 4-rra:rb
-void	move_node_sorted(t_node **stack_a, t_node **stack_b, unsigned long indexes, int op)
+void	move_node_sorted(t_node **s_a, t_node **s_b, unsigned long is, int op)
 {
 	int	ia;
 	int	ib;
 	int	i;
 
-	ia = right32bit(indexes);
-	ib = left32bit(indexes);
-	
+	ia = right32bit(is);
+	ib = left32bit(is);
 	i = 0;
 	if (op == 1)
-	{
-		while (i < min_of_2(ia, ib))
-		{
-			rotate_both(stack_a, stack_b);
-			write(1, "rr\n", 3);
-			i++;
-		}
-		while (i < ia)
-		{
-			rotate(stack_a);
-			write(1, "ra\n", 3);
-			i++;
-		}
-		while (i < ib)
-		{
-			rotate(stack_b);
-			write(1, "rb\n", 3);
-			i++;
-		}
-	}
+		move_node_op1(s_a, s_b, ia, ib);
 	else if (op == 2)
-	{
-		while (i < min_of_2(list_size(*stack_a) - ia, list_size(*stack_b) - ib))
-		{
-			reverse_rotate_both(stack_a, stack_b);
-			write(1, "rrr\n", 4);
-			i++;
-		}
-		while (i < list_size(*stack_a) - ia)
-		{
-			r_rotate(stack_a);
-			write(1, "rra\n", 4);
-			i++;
-		}
-		while (i < list_size(*stack_b) - ib)
-		{
-			r_rotate(stack_b);
-			write(1, "rrb\n", 4);
-			i++;
-		}
-	}
+		move_node_op2(s_a, s_b, ia, ib);
 	else if (op == 3)
-	{
-		while (i < ia)
-		{
-			rotate(stack_a);
-			write(1, "ra\n", 3);
-			i++;
-		}
-		i = 0;
-		while (i < list_size(*stack_b) - ib)
-		{
-			r_rotate(stack_b);
-			write(1, "rrb\n", 4);
-			i++;
-		}
-	}
+		move_node_op3(s_a, s_b, ia, ib);
 	else if (op == 4)
-	{
-		while (i < list_size(*stack_a) - ia)
-		{
-			r_rotate(stack_a);
-			write(1, "rra\n", 4);
-			i++;
-		}
-		i = 0;
-		while (i < ib)
-		{
-			rotate(stack_b);
-			write(1, "rb\n", 3);
-			i++;
-		}
-	}
+		move_node_op4(s_a, s_b, ia, ib);
+	push(s_a, s_b);
+	write(1, "pb\n", 3);
 }
 
 //returns the number of instructions, if fully sorted returns 0
-int	find_best_solution_apply_and_print(t_node **stack_a, t_node **stack_b)
+// function(stack_a, stack_b, 0, 0)
+int	apply_and_print(t_node **s_a, t_node **s_b, int i, unsigned long replace_a)
 {
-	unsigned long		replace_a;
 	unsigned long		replace_b;
-	int					i;
 	t_node				*temp;
-	int					as_place_in_b;
+	int					a_to_b;
 	unsigned long		min;
 	unsigned long		minn;
 
-	if (!*stack_a)
+	if (!*s_a)
 		return (0);
-	if (*stack_b == NULL && (*stack_a)->next)
-	{
-		push(stack_a, stack_b);
-		push(stack_a, stack_b);
-		write(1, "pb\npb\n", 6);
-		return (2);
-	}
-	temp = *stack_a;
-	replace_a = 0;
-	i = 0;
+	temp = *s_a;
 	min = INT_MAX;
 	while (temp)
 	{
-		as_place_in_b = find_place_for_number_in_list(*stack_b, temp->data);
-		minn = calculate_number_of_ops(list_size(*stack_a), i, list_size(*stack_b), as_place_in_b);
+		a_to_b = find_place_for_number_in_list(*s_b, temp->data);
+		minn = min_number_of_ops(list_size(*s_a), i, list_size(*s_b), a_to_b);
 		if (right32bit(minn) < right32bit(min))
 		{
 			min = minn;
 			replace_a = i;
-			replace_b = as_place_in_b;
+			replace_b = a_to_b;
 		}
 		i++;
 		temp = temp->next;
 	}
-	move_node_sorted(stack_a, stack_b, replace_a + (replace_b << 32), left32bit(min));
-	push(stack_a, stack_b);
-	write(1, "pb\n", 3);
+	move_node_sorted(s_a, s_b, replace_a + (replace_b << 32), left32bit(min));
 	return (min);
 }
 
 void	print_ops_for_linked_list(t_node **list)
 {
-	t_node	*stack_b;
+	t_node	*s_b;
 	int		total_instruction;
 
-	stack_b = NULL;
+	s_b = NULL;
 	if (list_size(*list) == 1)
 		return ;
-	while (*list)
-		find_best_solution_apply_and_print(list, &stack_b);
-	max_to_first_node(&stack_b);
-	while (stack_b)
+	if (s_b == NULL && (*list)->next)
 	{
-		push(&stack_b, list);
+		push(list, &s_b);
+		push(list, &s_b);
+		write(1, "pb\npb\n", 6);
+		return (2);
+	}
+	while (*list)
+		apply_and_print(list, &s_b, 0, 0);
+	max_to_first_node(&s_b);
+	while (s_b)
+	{
+		push(&s_b, list);
 		write(1, "pa\n", 3);
 	}
 	return ;
